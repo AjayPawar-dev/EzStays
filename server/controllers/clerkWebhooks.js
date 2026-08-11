a3a092557989c006df048003c68fcff240e1eccd
AjayPawar-dev <pawarajay2615@gmail.com>
2026-08-11 19:14:41 +0530

    Revert "minor typo fix"
    This reverts commit 059cfe33236b43e4927f5abfd90531388b94a282.

--- server/controllers/clerkWebhooks.js
+++ server/controllers/clerkWebhooks.js
@@ -1,55 +1,55 @@
 import User from "../models/User.js";
 import { Webhook } from "svix";
 
-const clerkWebhooks = async (req, res) => {
-    try {
-        // Create a svix instance with clerk webhook secret
-        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
+const clerkWebhooks = async (req,res)=>{
+    try{
+        //create a svix instance with clerk webhook secret.
+        const whook=  new Webhook(process.env.CLERK_WEBHOOK_SECRET)
 
-        // Getting headers
-        const headers = {
+        // getting headers
+        const headers ={
             "svix-id": req.headers["svix-id"],
-            "svix-timestamp": req.headers["svix-timestamp"],
-            "svix-signature": req.headers["svix-signature"],
+            "svix-timestamp":req.headers["svix-timestamp"],
+            "svix-signature":req.headers["svix-signature"],
+
         };
 
-        // Verifying Headers
-        await whook.verify(JSON.stringify(req.body), headers);
+        //Verfiying Headers
+        await whook.verify(JSON.stringify(req.body),headers)
 
-        // Getting Data from request body
-        const { data, type } = req.body;
+        //Getting Data from request body
+        const {data,type} =req.body
 
-        const userData = {
+        const userData={
             _id: data.id,
-            email: data.email_addresses[0].email_address,
-            username: data.first_name + " " + (data.last_name || ""),
+            email:data.email_addresses[0].email_address,
+            username: data.first_name + " " +data.last_name,
             image: data.image_url,
-            role: data.public_metadata?.role || "user"
-        };
-
-        // Switch cases for different events
-        switch (type) {
-            case "user.created": {
-                await User.create(userData);
+        }
+        //switch cases for different events
+        switch (key) {
+            case "user.created":{
+                await User.create(userData)
                 break;
             }
-            case "user.updated": {
-                await User.findByIdAndUpdate(data.id, userData);
+            case "user.updated":{
+                await User.findByIdAndUpdate(data.id,userData);
                 break;
             }
-            case "user.deleted": {
-                await User.findByIdAndDelete(data.id);
+            case "user.deleted":{
+                await User.findByIdAndDelete(data.id)
                 break;
             }
+        
             default:
                 break;
         }
+        res.json({sucess:true,message:"Webhook Recieved"})
 
-        return res.status(200).json({ success: true, message: "Webhook Received" });
 
-    } catch (error) {
+    } catch(error){
         console.log(error.message);
-        return res.status(400).json({ success: false, message: error.message });
+        res.json({sucess:false, message:error.message});
     }
 }
 
